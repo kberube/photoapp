d0aa7ae440d97f6c98df97ae51ae923c8e52da96
Keith Berube <keithberube@me.com>
2021-11-13 22:12:25 -0600

    added photos db elements

--- db/migrate/20211114040247_create_photos.rb
+++ db/migrate/20211114040247_create_photos.rb
@@ -2,9 +2,9 @@ class CreatePhotos < ActiveRecord::Migration[6.1]
   def change
     create_table :photos do |t|
       t.string :name
-      t.string :date
+      t.date :date
       t.int :rating
-      
+
       t.timestamps
     end
   end
